2dfc2e21981165c4daa98258462f12112b1d8ad4
yogicoderanne <darylanneking@gmail.com>
2020-06-10 17:12:05 -0500

    added mobilenet model

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [prediction, setPrediction] = useState(null)
+  const [loading, setLoading] = useState('loading...')
+  let mobileNetModel = null
+
+  useEffect(() => {
+    const loadMobilenet = async () => {
+      if (window.mobilenet) {
+        mobileNetModel = await window.mobilenet.load()
+        setLoading('Loaded!')
+      } else {
+        setTimeout(loadMobilenet(), 1000)
+      }
+    }
+    loadMobilenet()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>MobileNet Status: {loading}</p>
+        <p>Dog: Pug</p>
         <img src='/pug.jpg' width={325} height={250} alt='pug' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prediction: {prediction}
         </p>
       </header>
     </div>
